058fdcbe39e51976e824fe97ce08136514501698
Aashay Singhal <aashay225@gmail.com>
2017-06-12 12:16:53 +0530

    source file names found

---
+++ scripts/find.py
@@ -0,0 +1,40 @@
+with open("headers.txt") as f:
+	content = f.readlines()
+content = [x.strip() for x in content]
+
+# import re
+# allfile =[] 
+# for name in content:
+# 	pattern = re.compile("\".*\.H\"")
+
+	# for i, line in enumerate(open(name)):
+	# 	print "asd"
+	# 	for match in re.finditer(pattern, line):
+	# 		print 'Found on line %s: %s' % (i+1, match.groups())
+import subprocess,os,re
+final = []
+cmd = ['sudo', 'find' ,'/home/itsme/OpenFOAM/OpenFOAM-dev' ,'-print']
+output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+# result = pattern.findall(line)
+out = []
+buff = []
+for c in output:
+    if c == '\n':
+        out.append(''.join(buff))
+        buff = []
+    else:
+        buff.append(c)
+else:
+	if buff:
+    	out.append(''.join(buff))
+for line in content:
+	l = '/' + line
+	x = filter(lambda x: line in x, out)
+	final.append(x[1])
+# content = list(set(content))
+a = open("source.txt","w")
+# content.sort()
+for item in content:
+	print>>a, item
+
+# print allfile
